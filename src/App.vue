0c99b4836a36aee7f026ace9afaccde9fc30b7e9
N_Maser <niloofarmaser@gmail.com>
2020-08-23 21:19:02 -0400

    tempChange

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <h1>Hello World!</h1>
 </template>
-
-
+<script>
+export default {
+  
+}
+</script>
+<style>
 </style>
